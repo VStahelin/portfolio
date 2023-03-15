217fa8d56570d89d6494b027de3379da94272832
Vitor Stahelin <vitorjosestahelin@gmail.com>
2023-03-15 12:13:43 -0300

    Updates

---
+++ precache-manifest.17e93c1a997cb46045da7a4c3e7b3d61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fc917b6221290f11263",
+    "url": "/static/css/main.af034eb0.chunk.css"
+  },
+  {
+    "revision": "0fc917b6221290f11263",
+    "url": "/static/js/main.0fc917b6.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c889f73f9a88c8a7349609e74272d0e7",
+    "url": "/index.html"
+  }
+];
