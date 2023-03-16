f0d69eb511bad16fbec8029159fb2a9ca5d854b2
Vitor Stahelin <vitorjosestahelin@gmail.com>
2023-03-16 14:22:52 -0300

    Feat: configure first component test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import styles from './styles/styles.module.css'
-import { Container } from './components/atom'
+import IndexPage from './pages'
 
 function App (): JSX.Element {
   return (
-    <Container className={styles.container}>
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-    </Container>
+    <IndexPage />
   )
 }
 
