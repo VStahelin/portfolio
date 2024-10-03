f6a46e71c874fca467f4a8326ffea48adc725293
Vstahelin <vitorjosestahelin@gmail.com>
2024-10-03 17:54:35 -0300

    refactor: Update import path for Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/footer/index";
+import Footer from "./components/molecules/footer/index";
 import HomePage from "./components/pages/HomePage/HomePage";
 import AboutPage from "./components/pages/AboutPage/AboutPage";
 import { DataProvider } from "./context/DataAPIContext";
