6a81ae3fa4a2093ee3c7f8ced51352bbd348c4e0
Vstahelin <vitorjosestahelin@gmail.com>
2024-10-07 17:03:24 -0300

    refactor: Update construction notice position on homepage

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -18,7 +18,7 @@ const HomePage: React.FC = () => {
 
   return (
     <div className="bg-back min-h-screen">
-      <div className="fixed top-16 left-0 w-full bg-yellow-500 text-white text-center py-3 z-50">
+      <div className="fixed top-12 left-0 w-full bg-yellow-500 text-white text-center py-3 z-50">
         Esta página está sob construção.
       </div>
       <div id="navbar">
